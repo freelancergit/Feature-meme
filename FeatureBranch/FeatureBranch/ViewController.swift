b6961c749bd9cc36542aa52fd89f242a4e6e0517
podilivinaykumar <95670309+podilivinaykumar@users.noreply.github.com>
2021-12-07 10:51:48 +0530

    Feature2 added

--- FeatureBranch/FeatureBranch/ViewController.swift
+++ FeatureBranch/FeatureBranch/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Feature1 added")
+        print("Feature2 added")
         // Do any additional setup after loading the view.
     }
 
