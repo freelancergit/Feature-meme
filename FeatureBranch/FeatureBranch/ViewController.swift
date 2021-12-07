14ff20d4ccc19045c40a0aa30f275a987912dcec
podilivinaykumar <95670309+podilivinaykumar@users.noreply.github.com>
2021-12-07 10:55:09 +0530

    Main
    Main changes updated

--- FeatureBranch/FeatureBranch/ViewController.swift
+++ FeatureBranch/FeatureBranch/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Feature1 added")
         print("Feature2 added")
+        print("Main1 changes")
+        print("Main2 changes")
+        print("Main3 changes")
+        print("Main4 changes")
+        print("Main5 changes")
+        print("Main6 changes")
         // Do any additional setup after loading the view.
     }
 
